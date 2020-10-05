557e820cfbc437b9e772a9f87212d45d4b3ade2d
Vaqim <vadimser1355@gmail.com>
2020-10-05 16:52:43 +0300

    iteration method added

--- js/task2.js
+++ js/task2.js
@@ -1,5 +1,34 @@
 window.onload = function(){
     let X1, X2;
+    let dataSet = [{ // root is ~33.1
+        h: 2,
+        w: 1,
+        e: 5.3,
+    },{
+        h: 0.5,
+        w: 0.2,
+        e: 4.8,
+    },{
+        h: 1,
+        w: 0.3,
+        e: 5.5,
+    },{
+        h: 0.5,
+        w: 0.4,
+        e: 3.5,
+    },{
+        h: 0.5,
+        w: 0.1,
+        e: 4.3,
+    },{
+        h: 1,
+        w: 0.3,
+        e: 4.8,
+    },];
+
+    function roundInt(integer){
+        return (Math.round(integer * 10000) / 10000);
+    }
 
     function f(h, w, e1) {
         try {    
@@ -15,37 +44,44 @@ window.onload = function(){
         }
     }
 
+    function iteratorFunc(a, b, h, w, e) {
+        let result = {
+            start: {},
+            end: {}
+        };
+        let n = 0.0001;
+
+        for (let i = a; i < b; i += n){
+            let j = i;
+            if (f(h, w, j) === e || (f(h, w, j) - e) * (f(h, w, j + n) - e) < 0) {
+                result.start.y = j;
+                result.start.x = f(h, w, j);
+                result.end.y = j + n;
+                result.end.x = f(h, w, j + n);
+                return result;
+            }
+        }
+        return false;
+    }
+
     document.getElementById('drawIter').addEventListener('click', () => {
         X1 = +document.getElementById('x1').value;
         X2 = +document.getElementById('x2').value;
+        let dataSetNumber;
+
+        for (let i = 1; i <= 6; i++){
+            if (document.getElementById(`set${i}`).checked){
+                dataSetNumber = +i - 1;
+                break;
+            }
+        }
+        console.table(dataSet[dataSetNumber]);
         
-        let data = [{ // root is ~33.1
-                h: 2,
-                w: 1,
-                e: 5.3,
-            },{
-                h: 0.5,
-                w: 0.2,
-                e: 4.8,
-            },{
-                h: 1,
-                w: 0.3,
-                e: 5.5,
-            },{
-                h: 0.5,
-                w: 0.4,
-                e: 3.5,
-            },{
-                h: 0.5,
-                w: 0.1,
-                e: 4.3,
-            },{
-                h: 1,
-                w: 0.3,
-                e: 4.8,
-            },] 
-
-        console.log(f(data[0].h, data[0].w, 33.1) - data[0].e);
-
+        let results = iteratorFunc(X1, X2, dataSet[dataSetNumber].h, dataSet[dataSetNumber].w, dataSet[dataSetNumber].e);
+        if (!results) return;
+        let root = (results.end.y + results.start.y) / 2;
+        // console.log(f(dataSet[dataSetNumber].h, dataSet[dataSetNumber].w, root));
+        console.table(results);
+        console.log(`root: ${roundInt(root)}`);
     });
 };
