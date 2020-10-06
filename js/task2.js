809b34b8094c478b793843b33fd52f7b95d62350
Vaqim <vadimser1355@gmail.com>
2020-10-06 12:31:38 +0300

    graphics added

--- js/task2.js
+++ js/task2.js
@@ -31,6 +31,14 @@ window.onload = function(){
         return (Math.round(integer * 10000) / 10000);
     }
 
+    function getArrayRange(start, end, step){
+        let result = [];
+        for (let i = start; i <= end; i += step){
+            result.push(+i.toFixed(2));
+        }
+        return result;
+    }
+
     function f(h, w, e1) {
         try {    
             const part1 = (e1 + 1) / 2;
@@ -53,7 +61,7 @@ window.onload = function(){
         };
         let center;
 
-        let iterations = 50;
+        let iterations = 80;
 
         for(let i = 0; i < iterations; i++) {
 
@@ -69,18 +77,18 @@ window.onload = function(){
         // return [start, center, end];
 
         results.center.x = ((start + end) / 2);
-        results.center.y = (f(h, w, (start + end) / 2) - e);
+        results.center.y = (f(h, w, (start + end) / 2));
 
         if((f(h, w, (start + end) / 2) - e) * (f(h, w, end) - e) <= 0){
             results.green.x = end;
-            results.green.y = f(h, w, end) - e;
+            results.green.y = f(h, w, end);
             results.red.x = start;
-            results.red.y = f(h, w, start) - e;
+            results.red.y = f(h, w, start);
         } else if((f(h, w, (start + end) / 2) - e) * (f(h, w, start) - e) <= 0) {
             results.green.x = start;
-            results.green.y = f(h, w, start) - e;
+            results.green.y = f(h, w, start);
             results.red.x = end;
-            results.red.y = f(h, w, end) - e;
+            results.red.y = f(h, w, end);
         } else {
             return false;
         }
@@ -92,7 +100,7 @@ window.onload = function(){
             start: {},
             end: {}
         };
-        let n = 0.00001;
+        let n = 0.0001;
 
         for (let i = a; i < b; i += n){
             let j = i;
@@ -127,15 +135,169 @@ window.onload = function(){
         // console.log(f(dataSet[dataSetNumber].h, dataSet[dataSetNumber].w, root));
         console.table(results);
         console.log(`root: ${roundInt(root)}`);
+
+        const ctx = document.getElementById('iter').getContext('2d');
+
+        let data = {
+            labels: getArrayRange(X1, X2, 0.01),
+            datasets: [{
+                label: "f(x) ", // Name it as you want
+                function: function(x) { return +f(dataSet[dataSetNumber].h, dataSet[dataSetNumber].w, x).toFixed(2) },
+                data: [], // Don't forget to add an empty data array, or else it will break
+                borderColor: "rgba(75, 192, 192, 1)",
+                fill: false,
+                radius: 0,
+                order: 1
+            },
+            {
+                label: "Корінь", // Name it as you want
+                function: () => {},
+                data: [{
+                    x: +(results.start.y).toFixed(2),
+                    y: +(results.start.x).toFixed(2)
+                },
+                {
+                    x: +(results.end.y).toFixed(2),
+                    y: +(results.end.x).toFixed(2)
+                }], // Don't forget to add an empty data array, or else it will break
+                borderColor: "green",
+                borderWidth: 5,
+                order: 0,
+                fill: false,
+                radius: 2
+            }]
+        }
+
+        Chart.pluginService.register({
+            beforeInit: function(chart) {
+                // We get the chart data
+                let data = chart.config.data;
+        
+                // For every dataset ...
+                for (let i = 0; i < data.datasets.length; i++) {
+        
+                    // For every label ...
+                    for (let j = 0; j < data.labels.length; j++) {
+        
+                        // We get the dataset's function and calculate the value
+                        let fct = data.datasets[i].function,
+                            x = data.labels[j],
+                            y = fct(x);
+                        // Then we add the value to the dataset data
+                        data.datasets[i].data.push(y);
+                    }
+                }
+            }
+        });
+
+        let myChart = new Chart(ctx, {
+            type: 'line',
+            data: data,
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
     });
 
     document.getElementById('drawDuhot').addEventListener('click', () => {
         
-        let result = duhotFunc(X1, X2, dataSet[dataSetNumber].h, dataSet[dataSetNumber].w, dataSet[dataSetNumber].e);
-        console.table(result);
-        let root = (result.green.x + result.center.x) / 2
+        let results = duhotFunc(X1, X2, dataSet[dataSetNumber].h, dataSet[dataSetNumber].w, dataSet[dataSetNumber].e);
+        console.table(results);
+        if(!results) return;
+        let root = results.green.x;
 
         console.log(`root: ${roundInt(root)}`);
+
+        const ctx = document.getElementById('duhot').getContext('2d');
+
+        let data = {
+            labels: getArrayRange(X1, X2, 0.01),
+            datasets: [{
+                label: "f(x) ", // Name it as you want
+                function: x => {return +f(dataSet[dataSetNumber].h, dataSet[dataSetNumber].w, x).toFixed(2)},
+                data: [], // Don't forget to add an empty data array, or else it will break
+                borderColor: "rgba(75, 192, 192, 1)",
+                fill: false,
+                radius: 0,
+                order: 1
+            },{
+                label: "Корінь", // Name it as you want
+                function: () => {},
+                data: [{
+                    x: +(results.green.x).toFixed(2),
+                    y: +(results.green.y).toFixed(2)
+                },
+                {
+                    x: +(results.center.x).toFixed(2),
+                    y: +(results.center.y).toFixed(2)
+                }], // Don't forget to add an empty data array, or else it will break
+                borderColor: "green",
+                borderWidth: 5,
+                order: 0,
+                fill: false,
+                radius: 2,
+                order: 0
+            },{
+                label: "Не корінь", // Name it as you want
+                function: () => {},
+                data: [{
+                    x: +(results.red.x).toFixed(2),
+                    y: +(results.red.y).toFixed(2)
+                },
+                {
+                    x: +(results.center.x).toFixed(2),
+                    y: +(results.center.y).toFixed(2)
+                }], // Don't forget to add an empty data array, or else it will break
+                borderColor: "red",
+                borderWidth: 5,
+                order: 0,
+                fill: false,
+                radius: 2,
+                order: 0
+            }]
+        }
+
+        Chart.pluginService.register({
+            beforeInit: function(chart) {
+                // We get the chart data
+                let data = chart.config.data;
+        
+                // For every dataset ...
+                for (let i = 0; i < data.datasets.length; i++) {
+        
+                    // For every label ...
+                    for (let j = 0; j < data.labels.length; j++) {
+        
+                        // We get the dataset's function and calculate the value
+                        let fct = data.datasets[i].function,
+                            x = data.labels[j],
+                            y = fct(x);
+                        // Then we add the value to the dataset data
+                        data.datasets[i].data.push(y);
+                    }
+                }
+            }
+        });
+
+        let myBarChart = new Chart(ctx, {
+            type: 'line',
+            data: data,
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
         
         
     });
